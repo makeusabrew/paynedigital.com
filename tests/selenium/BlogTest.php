9448038e9e14fb748b18ea5faa7f3aed0fbe7338
Nick Payne <nick@kurai.co.uk>
2011-09-17 13:15:06 +0100

    fixing failing selenium tests (refs #320)

--- tests/selenium/BlogTest.php
+++ tests/selenium/BlogTest.php
@@ -24,13 +24,13 @@ class BlogTest extends SeleniumTestController {
     public function testHomepageShowsRecentPostsInCorrectOrder() {
         $this->open("/");
         // not massively keen on such tight DOM bounding, but let's see how it goes...
-        $this->assertElementPresent("//div[@id='posts']/div[@class='post'][1]/h2/a[text()='Another Test Post']");
-        $this->assertElementPresent("//div[@id='posts']/div[@class='post'][2]/h2/a[text()='This Is A Test Post']");
+        $this->assertElementPresent("//div[@id='posts']/div[@class='post'][1]//h2/a[text()='Another Test Post']");
+        $this->assertElementPresent("//div[@id='posts']/div[@class='post'][2]//h2/a[text()='This Is A Test Post']");
     }
 
     public function testPostLinksFromHomepageAreCorrect() {
         $this->open("/");
-        $this->assertElementPresent("//div[@id='posts']/div[@class='post'][1]/h2/a[@href='/2011/09/another-test-post']");
+        $this->assertElementPresent("//div[@id='posts']/div[@class='post'][1]//h2/a[@href='/2011/09/another-test-post']");
     }
 
     public function testStandalonePostPageTitle() {
