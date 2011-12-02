83c0f35bfad3721647f2baf6fbecea915d1f85aa
Nick Payne <nick@kurai.co.uk>
2011-12-02 10:34:42 +0000

    added shortlink for nodeflakes article

--- apps/shortlinks/controllers/shortlinks.php
+++ apps/shortlinks/controllers/shortlinks.php
@@ -11,6 +11,16 @@ class ShortlinksController extends Controller {
         ));
     }
 
+    public function nodeflakes_redirect() {
+        return $this->redirect(array(
+            "app"        => "blog",
+            "controller" => "Blog",
+            "action"     => "view_post",
+            "month"      => "2011/12",
+            "url"        => "nodeflakes",
+        ));
+    }
+
     public function trello_redirect() {
         return $this->redirect(Settings::getValue("trello.url"));
     }
--- apps/shortlinks/paths.php
+++ apps/shortlinks/paths.php
@@ -1,5 +1,6 @@
 <?php
 PathManager::loadPaths(
     array("/bootbox", "bootbox_redirect"),
+    array("/nodeflakes", "nodeflakes_redirect"),
     array("/trello", "trello_redirect")
 );
