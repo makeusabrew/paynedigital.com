9d6bed68fa15ccf880acbf265c31878729eaaf4c
Nick Payne <nick@kurai.co.uk>
2011-12-03 17:55:39 +0000

    adding rudimentary shortlinks test prior to re-work

---
+++ tests/apps/shortlinks/controllers/ShortlinksControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class ShortlinksControllerTest extends PHPUnitTestController {
+    /**
+     * @dataProvider shortLinksDP 
+     */
+    public function testShortLinks($url, $redirectUrl) {
+
+        $this->request->dispatch($url);
+        $this->assertResponseCode(303);
+        $this->assertController("Shortlinks");
+        $this->assertApp("shortlinks");
+        $this->assertRedirect(true);
+        $this->assertRedirectUrl($redirectUrl);
+    }
+
+    public function shortLinksDP() {
+        return array(
+            array("/bootbox", "/2011/11/bootbox-js-alert-confirm-dialogs-for-twitter-bootstrap"),
+            array("/trello",  "https://trello.com/board/paynedigital-com/4eba7ed4538fd4a54b302e23"),
+            array("/nodeflakes", "/2011/12/nodeflakes"),
+        );
+    }
+}
