0910713b815806387c90e5e1c0deb6a366c2dffa
Nick Payne <nick@kurai.co.uk>
2011-12-13 20:47:11 +0000

    reducing feed items to max of 5 (refs #675)

--- apps/feed/controllers/feed.php
+++ apps/feed/controllers/feed.php
@@ -3,7 +3,7 @@
 class FeedController extends Controller {
 
     public function index() {
-        $articles = Table::factory('Posts')->findRecent();
+        $articles = Table::factory('Posts')->findRecent(5);
         $this->assign('articles', $articles);
         $this->response->addHeader('Content-Type', 'application/xml; charset=utf-8'); 
     }
