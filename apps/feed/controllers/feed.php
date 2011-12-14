930eb7d52b03c06df4fed805110a9be2bd084dfa
Nick Payne <nick@kurai.co.uk>
2011-12-14 08:25:37 +0000

    a couple of rudimentary feed tests, changing feed header (refs #675)

--- apps/feed/controllers/feed.php
+++ apps/feed/controllers/feed.php
@@ -5,7 +5,7 @@ class FeedController extends Controller {
     public function index() {
         $articles = Table::factory('Posts')->findRecent(5);
         $this->assign('articles', $articles);
-        $this->response->addHeader('Content-Type', 'application/xml; charset=utf-8'); 
+        $this->response->addHeader('Content-Type', 'text/xml; charset=utf-8'); 
     }
 
 }
--- tests/apps/feed/controllers/FeedControllerTest.php
+++ tests/apps/feed/controllers/FeedControllerTest.php
@@ -5,4 +5,17 @@ class FeedControllerTest extends PHPUnitTestController {
         $this->request->dispatch("/feed.xml");
         $this->assertBodyHasContents("<title>Payne Digital</title>");
     }
+
+    public function testAlternateFeedLinkIsPresentOnHomepage() {
+        $this->request->dispatch("/");
+        $this->assertBodyHasContents('<link rel="alternate" type="application/rss+xml" title="Payne Digital RSS Feed" href="'.Settings::getValue('site.base_href').'feed.xml"/>');
+    }
+
+    public function testFeedSetsCorrectContentTypeHeader() {
+        $this->request->dispatch("/feed.xml");
+        $headers = $this->request->getResponse()->getHeaders();
+
+        $this->assertTrue(isset($headers['Content-Type']));
+        $this->assertEquals('text/xml; charset=utf-8', $headers['Content-Type']);
+    }
 }
