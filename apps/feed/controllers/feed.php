66ebc08b5614eac43780287fb4c559d94463cf94
Nick Payne <nick@kurai.co.uk>
2011-12-14 08:42:55 +0000

    bump recent articles up to 10 in the feed

--- apps/feed/controllers/feed.php
+++ apps/feed/controllers/feed.php
@@ -3,7 +3,7 @@
 class FeedController extends Controller {
 
     public function index() {
-        $articles = Table::factory('Posts')->findRecent(5);
+        $articles = Table::factory('Posts')->findRecent(10);
         $this->assign('articles', $articles);
         $this->response->addHeader('Content-Type', 'text/xml; charset=utf-8'); 
     }
