390dc95893cf7390d9b00be00a2e5517dea6ffac
Nick Payne <nick@kurai.co.uk>
2012-01-24 11:00:17 +0000

    fixing broken test logic

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -41,6 +41,11 @@ include("library/utils.php");
 include("library/image.php");
 include("library/cache.php");
 
+/**
+ * custom PD stuff (@todo: autoloader!!)
+ */
+include("apps/stats/statsd.php");
+
 $mode = getenv("PROJECT_MODE") !== false ? getenv("PROJECT_MODE") : "test";
 Settings::setMode($mode);
 
