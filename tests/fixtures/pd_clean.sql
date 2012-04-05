a2e9915a4d5df28df2aadfee6ed5ea2cfac92aa5
Nick Payne <nick@kurai.co.uk>
2012-04-05 08:37:08 +0100

    adding test post to fixture for comments work (refs #889)

--- tests/fixtures/pd_clean.sql
+++ tests/fixtures/pd_clean.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.58, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.20, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: paynedigital_test
 -- ------------------------------------------------------
--- Server version	5.1.58-1ubuntu1
+-- Server version	5.5.20-0~ppa1~oneiric-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -105,7 +105,7 @@ CREATE TABLE `posts` (
   KEY `published` (`published`),
   KEY `status` (`status`),
   KEY `tags` (`tags`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +114,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,1,'This Is A Test Post','this-is-a-test-post','<p>This is simply a test post.</p>\r\n<p>It doesn\'t do <strong>much</strong> <em>at</em> <small>all</small>.','<p>This is simply a test post.</p>\r\n<p>It doesn\'t do <strong>much</strong> <em>at</em> <small>all</small>.','2011-09-14 17:34:41','PUBLISHED','2011-09-14 17:34:41','2011-09-14 17:34:41','|web|apache|music|test|','<link rel=\"stylesheet\" type=\"text/css\" href=\"/foo/bar.css\" />','<script type=\"text/javascript\" src=\"/foo/bar.js\"></script>'),(2,1,'This post hasn\'t been published','not-published-yet','<p>This post hasn\'t been published yet.</p>','<p>This post hasn\'t been published yet.</p>','2010-01-01 12:00:00','DRAFT','2011-09-14 17:34:41','2011-09-14 17:34:41','|apache|test|','',''),(3,2,'Another Test Post','another-test-post','<p>This test post doesn\'t do much either - it\'s only here to test that posts appear in the correct order.</p>','<p>This test post doesn\'t do much either - it\'s only here to test that posts appear in the correct order.</p>','2011-09-14 18:13:47','PUBLISHED','2011-09-14 18:13:47','2011-09-14 18:13:47','|test|php|apache|','',''),(4,1,'This Post Has Been Deleted','this-post-has-been-deleted','<p>Oh well. Nevermind.</p>','<p>Oh well. Nevermind.</p>','2011-09-15 14:43:11','DELETED','2011-09-15 14:43:11','2011-09-15 14:43:11','|test|','',''),(5,1,'This post will be published in the future','this-post-will-be-published-in-future','<p>This is a test post to check that posts marked with a status of \'published\' don\'t appear until their published date.</p>','<p>This is a test post to check that posts marked with a status of \'published\' don\'t appear until their published date.</p>','2021-01-01 00:00:00','PUBLISHED','2011-09-17 11:40:16','2011-09-17 11:40:16','|test|published|','',''),(6,2,'Testing Tags','testing-tags','<p>This post just tests tags with spaces.</p>','<p>This post just tests tags with spaces.</p>','2011-09-11 12:32:08','PUBLISHED','2011-09-11 12:32:08','2011-09-11 12:32:08','|test|Server Administration|node.js|','',''),(7,2,'Just A Test','just-a-test','<p>This is just a test post. It has no tags.</p>','<p>This is just a test post. It has no tags.</p>','2011-07-01 00:00:00','PUBLISHED','2011-07-01 00:00:00','2011-07-01 00:00:00','','','');
+INSERT INTO `posts` VALUES (1,1,'This Is A Test Post','this-is-a-test-post','<p>This is simply a test post.</p>\r\n<p>It doesn\'t do <strong>much</strong> <em>at</em> <small>all</small>.','<p>This is simply a test post.</p>\r\n<p>It doesn\'t do <strong>much</strong> <em>at</em> <small>all</small>.','2011-09-14 17:34:41','PUBLISHED','2011-09-14 17:34:41','2011-09-14 17:34:41','|web|apache|music|test|','<link rel=\"stylesheet\" type=\"text/css\" href=\"/foo/bar.css\" />','<script type=\"text/javascript\" src=\"/foo/bar.js\"></script>'),(2,1,'This post hasn\'t been published','not-published-yet','<p>This post hasn\'t been published yet.</p>','<p>This post hasn\'t been published yet.</p>','2010-01-01 12:00:00','DRAFT','2011-09-14 17:34:41','2011-09-14 17:34:41','|apache|test|','',''),(3,2,'Another Test Post','another-test-post','<p>This test post doesn\'t do much either - it\'s only here to test that posts appear in the correct order.</p>','<p>This test post doesn\'t do much either - it\'s only here to test that posts appear in the correct order.</p>','2011-09-14 18:13:47','PUBLISHED','2011-09-14 18:13:47','2011-09-14 18:13:47','|test|php|apache|','',''),(4,1,'This Post Has Been Deleted','this-post-has-been-deleted','<p>Oh well. Nevermind.</p>','<p>Oh well. Nevermind.</p>','2011-09-15 14:43:11','DELETED','2011-09-15 14:43:11','2011-09-15 14:43:11','|test|','',''),(5,1,'This post will be published in the future','this-post-will-be-published-in-future','<p>This is a test post to check that posts marked with a status of \'published\' don\'t appear until their published date.</p>','<p>This is a test post to check that posts marked with a status of \'published\' don\'t appear until their published date.</p>','2021-01-01 00:00:00','PUBLISHED','2011-09-17 11:40:16','2011-09-17 11:40:16','|test|published|','',''),(6,2,'Testing Tags','testing-tags','<p>This post just tests tags with spaces.</p>','<p>This post just tests tags with spaces.</p>','2011-09-11 12:32:08','PUBLISHED','2011-09-11 12:32:08','2011-09-11 12:32:08','|test|Server Administration|node.js|','',''),(7,2,'Just A Test','just-a-test','<p>This is just a test post. It has no tags.</p>','<p>This is just a test post. It has no tags.</p>','2011-07-01 00:00:00','PUBLISHED','2011-07-01 00:00:00','2011-07-01 00:00:00','','',''),(8,1,'A test post for comments','test-post-for-comments','<p>This is simply a test post.</p>\r\n<p>It doesn\'t do <strong>much</strong> <em>at</em> <small>all</small>.','<p>This is simply a test post.</p>\r\n<p>It doesn\'t do <strong>much</strong> <em>at</em> <small>all</small>.','2011-03-11 17:34:41','PUBLISHED','2011-03-11 17:34:41','2011-03-11 17:34:41','|comments|','','');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -279,4 +279,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-01-25 21:22:17
+-- Dump completed on 2012-04-05  8:33:02
