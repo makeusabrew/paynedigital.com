7b0a980ad87e5cfeb9a0ab80f531221c92a36260
Nick Payne <nick@kurai.co.uk>
2012-08-20 09:58:34 +0100

    render explicit response in feed controller

--- apps/feed/controllers/feed.php
+++ apps/feed/controllers/feed.php
@@ -5,7 +5,10 @@ class FeedController extends Controller {
     public function index() {
         $articles = Table::factory('Posts')->findRecent(10);
         $this->assign('articles', $articles);
+
         $this->response->addHeader('Content-Type', 'text/xml; charset=utf-8'); 
+
+        return $this->render("index");
     }
 
 }
